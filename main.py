17f18ce588ff9f44fe5e3a38b3c093504e6a41be
kovskiy <stanislavbankovskiy@gmail.com>
2026-01-20 01:23:46 +0200

    added event listener

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from hyprpy import Hyprland
+from hyprpy.utils.shell import run_or_fail
+import time
 
-instance = Hyprland()
+trigger = "discord"
+
+inst= Hyprland()
+
+#while (True):
+#    var = instance.get_active_window()
+#    print(var.wm_class)
+#    time.sleep(0.5)
+
+def super_window(sender, **kwargs):
+    print("event kwargs: ", kwargs)
+
+    win = inst.get_active_window()
+    if win.wm_class == trigger:
+        print("yay")
+
+inst.signals.activewindow.connect(super_window)
+
+inst.watch()
